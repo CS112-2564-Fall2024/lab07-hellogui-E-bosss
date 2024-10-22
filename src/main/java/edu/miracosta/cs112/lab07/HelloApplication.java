b327433f0dee0f8f41648b549b50c5b4abc8bb1e
Eseng <esenger02@icloud.com>
2024-10-21 22:17:00 -0700

    Update

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,8 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
@@ -18,19 +20,26 @@ public class HelloApplication extends Application { //inheriting core functional
         launch(args); //method from Application class, must be called to setup javafx application
     }
 
+    // TODO: follow steps 2-9 in README.md to create a start method
     public void start(Stage primaryStage) {
-        primaryStage = setTitle("Hello GUI: Ethan Senger");
+        primaryStage.setTitle("Hello GUI: Ethan Senger");
 
         StackPane layout = new StackPane();
+
+        Label label = new Label();
+        label.setText("Hello GUI World");
+
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setBottomAnchor(label, 0.0);
+        anchorPane.setRightAnchor(label, 0.0);
+
+        anchorPane.getChildren().add(label);
+        layout.getChildren().add(anchorPane);
 
         Scene scene = new Scene(layout, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-
-    // TODO: follow steps 2-9 in README.md to create a start method
-
     // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
 
     // TODO: follow steps 22-34 in README.md to create an event handler
